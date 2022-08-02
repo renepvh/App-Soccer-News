f0b5ed2f885b606e5a7d76bf6a9aa060d13a4e7d
Renê Alves <renepvh11@gmail.com>
2022-08-02 09:38:45 -0400

    Inclusão do Room e funcionalidades de 'Share' e 'Favorite'

--- app/src/main/java/me/dio/soccernews/ui/favorites/FavoritesFragment.java
+++ app/src/main/java/me/dio/soccernews/ui/favorites/FavoritesFragment.java
@@ -9,8 +9,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import java.util.List;
+
+import me.dio.soccernews.MainActivity;
 import me.dio.soccernews.databinding.FragmentFavoritesBinding;
+import me.dio.soccernews.domain.News;
+import me.dio.soccernews.ui.adapter.NewsAdapter;
 
 public class FavoritesFragment extends Fragment {
 
@@ -22,10 +28,24 @@ public class FavoritesFragment extends Fragment {
                 new ViewModelProvider(this).get(FavoritesViewModel.class);
 
         binding = FragmentFavoritesBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        favoritesViewModel.getText().observe(getViewLifecycleOwner(), binding.textFavorites::setText);
-        return root;
+
+        loadFavoriteNews();
+
+        return binding.getRoot();
+    }
+
+    private void loadFavoriteNews() {
+        MainActivity activity = (MainActivity) getActivity();
+        if (activity != null) {
+            List<News> favoriteNews = activity.getDb().NewsDao().loadFavoriteNews();
+            binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
+            binding.rvNews.setAdapter(new NewsAdapter(favoriteNews, updatedNews -> {
+                activity.getDb().NewsDao().save(updatedNews);
+                loadFavoriteNews();
+            }));
+        }
+
     }
 
     @Override
--- app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
@@ -4,19 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import me.dio.soccernews.MainActivity;
+import me.dio.soccernews.data.local.AppDatabase;
 import me.dio.soccernews.databinding.FragmentNewsBinding;
 import me.dio.soccernews.ui.adapter.NewsAdapter;
 
 public class NewsFragment extends Fragment {
 
     private FragmentNewsBinding binding;
+    private  AppDatabase db;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,10 +29,31 @@ public class NewsFragment extends Fragment {
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+
         binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
         newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
-            binding.rvNews.setAdapter(new NewsAdapter(news));
+            binding.rvNews.setAdapter(new NewsAdapter(news, updateNews -> {
+                MainActivity activity = (MainActivity) getActivity();
+                if (activity != null) {
+                    activity.getDb().NewsDao().save(updateNews);
+                }
+            }));
         });
+
+        newsViewModel.getState().observe(getViewLifecycleOwner(), state -> {
+            switch (state) {
+                case DOING:
+                    //TODO: Incluir SwipeRefresLayout (loading)
+                    break;
+                case DONE:
+                    //TODO: Finalizar SwipeRefresLayout (loading)
+                    break;
+                case ERROR:
+                    //TODO: Finalizar SwipeRefresLayout (loading)
+                    //TODO: Mostrar um erro generico.
+            }
+        });
+
         return root;
     }
 
